51006fe1e4862ca12b4e0a0026064b5b1fd6a26b
12cwittwer <12christianw@gmail.com>
2025-04-08 20:45:25 -0600

    LOGS

--- include/raytracer/device_ray_color.h
+++ include/raytracer/device_ray_color.h
@@ -15,12 +15,10 @@ __device__ color ray_color(
     if (depth <= 0)
         return color(0, 0, 0);
 
-    if (depth == 49) {
-        printf("ray_color origin=(%.2f,%.2f,%.2f)\n",
-                r.origin().x(), r.origin().y(), r.origin().z());
-    }
-
     hit_record rec;
+    if (depth == 49 && rec.hit) {
+        printf("hit at t=%.2f, mat type=%d, mat_ptr=%p\n", rec.t, (int)rec.mat_ptr->type, rec.mat_ptr->data);
+    }
     interval t_range(0.001, infinity);
     rec = hit_hittable(*world, r, t_range, rec);
     if (!rec.hit)
@@ -29,7 +27,9 @@ __device__ color ray_color(
     ray scattered;
     color attenuation;
     color emitted = emitted_material(*rec.mat_ptr, rec.u, rec.v, rec.p);
-
+    if (depth == 49) {
+        printf("Scattering material type=%d\n", (int)rec.mat_ptr->type);
+    }
     if (!scatter_material(*rec.mat_ptr, r, rec, attenuation, scattered, &rng))
         return emitted;
 
