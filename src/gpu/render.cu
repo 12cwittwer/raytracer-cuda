173ba7da1f63288b3d813aff1051b0e8fa04f750
12cwittwer <12christianw@gmail.com>
2025-04-10 17:35:22 -0600

    Print

--- src/gpu/render.cu
+++ src/gpu/render.cu
@@ -82,6 +82,7 @@ void launch_render_kernel(const camera_data* cam, const hittable* world, color* 
 
     // Launch kernel
     // Try rendering the first pixel
+    printf("Launching kernel\n")
     render_kernel<<<1, 1>>>(cam, world, fb);
     render_kernel<<<num_blocks, threads_per_block>>>(cam, world, fb);
 
